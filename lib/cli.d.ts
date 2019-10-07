d4157906bff8d03fffbdb11d974441862d761f2e
guanbinrui <guanbinrui@dimension.im>
2019-10-07 23:47:13 +0800

    :package: 0.3.7

--- lib/cli.d.ts
+++ lib/cli.d.ts
@@ -1,22 +1,2 @@
-import { Result } from 'meow';
-import { GrayscaleAlgorithm } from './grayscale';
-import { TransformAlgorithm } from './transform';
-import { EncodeOptions, DecodeOptions } from '.';
-export interface Flags {
-    help: boolean;
-    version: boolean;
-    encode: boolean;
-    decode: boolean;
-    message: string;
-    size: number;
-    copies: number;
-    pass?: string;
-    tolerance: number;
-    grayscale: GrayscaleAlgorithm;
-    transform: TransformAlgorithm;
-}
-export declare function normalize(flags: Result['flags']): Flags;
-export declare function validate({ encode, message, size, copies, tolerance, grayscale, transform, }: Flags): "" | "-m, --message is required" | "-s, --size should be a postive radix-2 number" | "-c, --copies should be a postive odd number" | "-t, --tolerance should be a positive number between [0-128]" | "unknown grayscale algorithm" | "unknown transform algorithm";
-export declare function flags2Options({ message, size, pass, copies, tolerance, grayscale, transform, }: Flags): EncodeOptions & DecodeOptions;
 export declare function run(): Promise<void>;
 export declare const version: string;
---
+++ lib/constant.d.ts
@@ -0,0 +1,5 @@
+export declare const CLI_NAME = "stego";
+export declare const DEFAULT_CLIP = 0;
+export declare const DEFAULT_COPIES = 3;
+export declare const DEFAULT_SIZE = 8;
+export declare const DEFAULT_TOLERANCE = 128;
