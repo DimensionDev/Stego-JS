79bb452569ba89bb33d4ec6353c20230b967b947
Septs <github@septs.pw>
2020-11-02 15:54:58 +0800

    remove scripts

--- scripts/dct.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2002
-FILE_NAME="$1"
-
-npm run build
-cat "$FILE_NAME" | stego-js -e -m "$(pbpaste)" -f DCT 2>test.log 1>test.png
-cat test.png | stego-js -d -f DCT
--- scripts/fft2d.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2002
-FILE_NAME="$1"
-
-npm run build
-cat "$FILE_NAME" | stego-js -e -m "$(pbpaste)" -f FFT2D 2>test.log 1>test.png
-cat test.png | stego-js -d -f FFT2D
