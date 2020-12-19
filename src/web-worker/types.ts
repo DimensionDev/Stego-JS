e9c8c2d70bdf1a9fc31741120d34b05e2a7a7a0e
Septs <github@septs.pw>
2020-12-19 19:05:10 +0800

    add web worker support (draft)

---
+++ src/web-worker/types.ts
@@ -0,0 +1,25 @@
+import { DecodeOptions, EncodeOptions } from '../utils/stego-params'
+
+export type Payload = EncodePayload | DecodePayload
+
+export interface EncodePayload {
+  type: 'encode'
+  id: unknown
+  imgData: WorkerImageData
+  maskData: WorkerImageData
+  options: EncodeOptions
+}
+
+export interface DecodePayload {
+  type: 'decode'
+  id: unknown
+  imgData: WorkerImageData
+  maskData: WorkerImageData
+  options: DecodeOptions
+}
+
+export interface WorkerImageData {
+  data: Uint8ClampedArray
+  width: number
+  height: number
+}
