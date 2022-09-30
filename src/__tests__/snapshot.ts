6eef5f0359d46579a447ffff5b91c7fb84d9aefc
guanbinrui <guanbinrui@dimension.im>
2022-09-30 12:49:44 +0800

    test: update test cases

--- src/__tests__/snapshot.ts
+++ src/__tests__/snapshot.ts
@@ -1,11 +1,12 @@
 import { readFile } from 'fs/promises'
 import { toMatchFile } from 'jest-file-snapshot'
 import { join } from 'path'
-import { encode, decode, TransformAlgorithm, GrayscaleAlgorithm, AlgorithmVersion } from '../../node'
+import { encode, decode, TransformAlgorithm, GrayscaleAlgorithm, AlgorithmVersion } from '../node'
 expect.extend({ toMatchFile })
 
 const original = readFile(join(__dirname, './original.png'))
 const mask = readFile(join(__dirname, './mask.png'))
+const transparent = readFile(join(__dirname, './mask-transparent.png'))
 const snapshot1 = join(__dirname, './__file__snapshot__/v1-snapshot-0.png')
 const snapshot2 = join(__dirname, './__file__snapshot__/v2-snapshot-0.png')
 const text =
@@ -42,11 +43,12 @@ test('v1', async () => {
 })
 
 test('v2', async () => {
-  const outImage = await encode(await original, await mask, {
+  const outImage = await encode(await original, await transparent, {
+    version: AlgorithmVersion.V2,
     size: 8,
     narrow: 0,
     copies: 3,
-    tolerance: 3,
+    tolerance: 400,
     exhaustPixels: false,
     cropEdgePixels: false,
     fakeMaskPixels: false,
@@ -54,19 +56,19 @@ test('v2', async () => {
     transformAlgorithm: TransformAlgorithm.FFT2D,
     pass: 'Hello World',
     text: text,
-    version: AlgorithmVersion.V2,
   })
+
   // Note: the algorithm currently is non-deterministic.
   // TODO: refactor to move random source out.
   // expect(outImage).toMatchFile(snapshot2)
 
-  const decodedText = await decode(outImage, await mask, {
+  const decodedText = await decode(outImage, await transparent, {
+    version: AlgorithmVersion.V2,
     size: 8,
     copies: 3,
-    tolerance: 128,
-    transformAlgorithm: TransformAlgorithm.FFT1D,
+    tolerance: 400,
+    transformAlgorithm: TransformAlgorithm.FFT2D,
     pass: 'Hello World',
-    version: AlgorithmVersion.V2,
   })
   expect(decodedText).toBe(text)
 })
