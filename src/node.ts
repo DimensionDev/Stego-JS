4d94a3fff0df9be3ad6def20aca883beed8db1f9
Jack Works <jackworks@protonmail.com>
2022-10-09 14:21:28 +0800

    feat: replace node-canvas with napi-rs (#34)
    * test: add old picture compatibility test
    
    * chore: replace node-canvas with napi-rs

--- src/node.ts
+++ src/node.ts
@@ -1,4 +1,4 @@
-import { createCanvas, Image } from 'canvas'
+import { JsColorType, Transformer } from '@napi-rs/image'
 import { proxy } from './utils/expose'
 import { imgType } from './utils/helper'
 import { preprocessImage } from './utils/image'
@@ -13,31 +13,53 @@ export * from './constant'
 const { encode, decode } = proxy({
   algoithms: { [AlgorithmVersion.V1]: v1, [AlgorithmVersion.V2]: v2 },
   methods: {
-    toImageData(data) {
-      return new Promise((resolve, reject) => {
-        const element = new Image()
-        element.onload = () => {
-          const { width, height } = element
-          const ctx = createCanvas(width, height).getContext('2d')!
-          ctx.drawImage(element, 0, 0, width, height)
-          resolve(ctx.getImageData(0, 0, width, height))
-        }
-        element.onerror = reject
-        element.src = Buffer.from(data)
-      })
+    async toImageData(data) {
+      let transformer = new Transformer(Buffer.from(data))
+      let { width, height, colorType } = await transformer.metadata()
+
+      if (colorType !== JsColorType.Rgba8 && colorType !== JsColorType.Rgb8) {
+        transformer = new Transformer(await transformer.png())
+        ;({ width, height, colorType } = await transformer.metadata())
+      }
+
+      if (colorType !== JsColorType.Rgba8 && colorType !== JsColorType.Rgb8) {
+        throw new TypeError('Cannot convert the given image to rgba8 format.')
+      }
+      let rgb: Uint8ClampedArray = new Uint8ClampedArray(await transformer.rawPixels())
+      if (colorType === JsColorType.Rgb8) rgb = rgb_to_rgba(rgb)
+
+      const imageData: ImageData = {
+        width,
+        height,
+        colorSpace: 'srgb',
+        data: rgb,
+      }
+      return imageData
     },
     async toBuffer(imgData, height = imgData.height, width = imgData.width) {
-      const canvas = createCanvas(width, height)
-      canvas.getContext('2d').putImageData(imgData, 0, 0, 0, 0, width, height)
-      return canvas.toBuffer('image/png')
+      return (await Transformer.fromRgbaPixels(imgData.data, width, height).crop(0, 0, width, height).png()).buffer
     },
     preprocessImage(data) {
-      return preprocessImage(
-        data,
-        (width, height) => createCanvas(width, height).getContext('2d')?.createImageData(width, height) ?? null,
-      )
+      return preprocessImage(data, (width, height) => ({
+        height,
+        width,
+        colorSpace: 'srgb',
+        data: new Uint8ClampedArray(width * height * 4),
+      }))
     },
   },
 })
 
 export { encode, decode }
+
+function rgb_to_rgba(array: Uint8ClampedArray) {
+  const next = new Uint8ClampedArray((array.length / 3) * 4)
+  for (var old_index = 0, new_index = 0; old_index < array.length; old_index += 3, new_index += 4) {
+    next[new_index] = array[old_index]
+    next[new_index + 1] = array[old_index + 1]
+    next[new_index + 2] = array[old_index + 2]
+    next[new_index + 3] = 255
+  }
+  if (new_index !== next.length) throw new Error('rgb_to_rgba error')
+  return next
+}
